2782c49d1fa780a793530025388207d48882f0b9
Taeyeon Kim <taekim1291@gmail.com>
2025-10-27 15:50:45 -0600

    coffee_machine

---
+++ Coffee Machine/coffee_machine.py
@@ -0,0 +1,38 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+beverage_options = input("What would you like? [Espresso / Latte / Cappuccino]: \n")
+print(beverage_options)
+
+turn_coffee_off = input("Done ordering? Type ['off']: \n").lower()
+
+
