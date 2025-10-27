db0194c0a33954d11139800f3f8b6549eb301d09
Taeyeon Kim <taekim1291@gmail.com>
2025-10-27 16:27:49 -0600

    coffee machine - coffee_machine.py

--- Coffee Machine/coffee_machine.py
+++ Coffee Machine/coffee_machine.py
@@ -27,12 +27,18 @@ MENU = {
 resources = {
     "water": 300,
     "milk": 200,
-    "coffee": 100,
+    "coffee beans": 100,
 }
 
-beverage_options = input("What would you like? [Espresso / Latte / Cappuccino]: \n")
-print(beverage_options)
+beverage_options = input("What would you like? [Espresso / Latte / Cappuccino]: ")
+def format_data(resources):
+    resource_one = resources["water"]
+    resource_two = resources["milk"]
+    resource_three = resources["coffee beans"]
+    if beverage_options == "report":
+        print(f"Water: {resource_one}ml \nMilk: {resource_two}ml \nCoffee Beans:{resource_three}g")
+
+format_data(resources)
 
 turn_coffee_off = input("Done ordering? Type ['off']: \n").lower()
-
 
