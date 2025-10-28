73c56c9e28fce90b612c920fa6687a6d5adefae3
Taeyeon Kim <taekim1291@gmail.com>
2025-10-28 14:23:48 -0600

    day-16-start

--- PythonProject/day-16-start.py
+++ PythonProject/day-16-start.py
@@ -1,17 +1,17 @@
 
 
-# from turtle import Turtle, Screen
-# timmy = Turtle()
-# print(timmy)
-# timmy.shape("turtle")
-# timmy.pencolor("rosybrown4")
-# timmy.fillcolor("rosybrown3")
-# timmy.forward(100)
-#
-# my_screen = Screen()
-# print(my_screen.canvheight)
-# print(my_screen.canvwidth)
-# my_screen.exitonclick()
+from turtle import Turtle, Screen
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.pencolor("rosybrown4")
+timmy.fillcolor("rosybrown3")
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+print(my_screen.canvwidth)
+my_screen.exitonclick()
 
 from prettytable import PrettyTable
 table = PrettyTable()
