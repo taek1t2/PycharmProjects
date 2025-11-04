42159ae16b22b600733378a626d2cf3fd88a937e
Taeyeon Kim <taekim1291@gmail.com>
2025-11-04 15:39:19 -0700

    colorgram_extractor.py

--- 100 Days of Code - The Complete Python Pro Bootcamp/day-18-start/colorgram_extractor.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/day-18-start/colorgram_extractor.py
@@ -4,8 +4,8 @@
 # colors = colorgram.extract('image.jpg', 45)
 # for color in colors:
 #     r = color.rgb.r
-#     g = color.rgb.r
-#     b = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
 #     new_color = (r, g, b)
 #     rgb_colors.append(new_color)
 #
@@ -16,14 +16,35 @@ import random
 
 tae = t.Turtle()
 tae.shape("turtle")
+tae.speed("fastest")
+t.colormode(255)
+tae.penup()
+tae.goto(-200,-100)
+
+color_list = [(249, 248, 248), (232, 241, 239), (1, 10, 30), (229, 235, 242), (239, 232, 238), (122, 95, 41), (71, 31, 21), (238, 212, 72), (220, 81, 59), (226, 117, 100), (93, 1, 21), (178, 140, 171), (151, 92, 115), (35, 90, 26), (7, 154, 72), (205, 63, 91), (221, 178, 218), (168, 129, 77), (1, 64, 147), (3, 79, 29), (4, 220, 218), (80, 135, 179), (132, 158, 177), (81, 110, 136), (116, 187, 164), (11, 215, 222), (117, 19, 37), (131, 224, 209), (230, 173, 165), (243, 205, 7), (70, 72, 44), (186, 190, 199), (91, 48, 43), (128, 223, 230)]
+dot_size = 20
+
+def draw_row(num_dots):
+    for dot in range(num_dots):
+        tae.dot(dot_size, random.choice(color_list))
+        tae.penup()
+        tae.fd(50)
 
 
+def to_turn():
+    draw_row(10)
+    tae.setheading(90)
+    draw_row(1)
+    tae.left(90)
+    draw_row(10)
 
-color_list = [(232, 232, 232), (1, 1, 1), (229, 229, 229), (239, 239, 239), (122, 122, 122), (71, 71, 71), (238, 238, 238), (220, 220, 220), (226, 226, 226), (93, 93, 93), (178, 178, 178), (151, 151, 151), (35, 35, 35), (7, 7, 7), (205, 205, 205), (221, 221, 221), (168, 168, 168), (1, 1, 1), (3, 3, 3), (4, 4, 4), (80, 80, 80), (132, 132, 132), (81, 81, 81), (116, 116, 116), (11, 11, 11), (117, 117, 117), (131, 131, 131), (230, 230, 230), (243, 243, 243), (70, 70, 70), (186, 186, 186), (91, 91, 91), (128, 128, 128)]
+to_turn()
+
+
+screen = t.Screen()
+screen.exitonclick()
+
 
 # 10 x 10 random dots
 # 20 in size of the circle
 # 50 paces apart
-
-screen = t.Screen()
-screen.exitonclick()
