26fed1a3b4f829939a6422dfce848f50dfd17756
Taeyeon Kim <taekim1291@gmail.com>
2025-11-10 16:03:57 -0700

    main, paddle

--- 100 Days of Code - The Complete Python Pro Bootcamp/day-22-start/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/day-22-start/main.py
@@ -7,12 +7,15 @@ screen.bgcolor("black")
 screen.title("Ping Pong")
 screen.tracer(0)
 
-
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
 
 screen.listen()
-screen.onkey(go_up, 'Up')
-screen.onkey(go_down, 'Down')
+screen.onkey(r_paddle.go_up, 'Up')
+screen.onkey(r_paddle.go_down, 'Down')
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
--- 100 Days of Code - The Complete Python Pro Bootcamp/day-22-start/paddle.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/day-22-start/paddle.py
@@ -3,20 +3,18 @@ from turtle import Turtle
 
 class Paddle(Turtle):
 
-    def __init__(self):
+    def __init__(self, position):
         super().__init__()
         self.shape("square")
-        paddle.color("white")
-        paddle.shapesize(stretch_wid=5, stretch_len=1)
-        paddle.penup()
-        paddle.setpos(350, 0)
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.setpos(position)
 
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
 
-
-    def go_up():
-        new_y = paddle.ycor() + 20
-        paddle.goto(paddle.xcor(), new_y)
-
-    def go_down():
-        new_y = paddle.ycor() - 20
-        paddle.goto(paddle.xcor(), new_y)
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
