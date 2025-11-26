a1dd6800a6a0803b069a902933dfe817c5359c36
Taeyeon Kim <taekim1291@gmail.com>
2025-11-26 13:30:15 -0500

    main

--- 100 Days of Code - The Complete Python Pro Bootcamp/day-25-start/day-25-us-states-game-start/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/day-25-start/day-25-us-states-game-start/main.py
@@ -9,20 +9,24 @@ turtle.shape(image)
 
 
 
-def get_mouse_click_coor(x, y):
-    print(x, y)
 
-turtle.onscreenclick(get_mouse_click_coor)
 
-state_answer = screen.textinput(title= "Guess the State", prompt="What's another state's name?")
+state_answer = screen.textinput(title= "Guess the State",
+                                prompt="What's another state's name?".title())
 print(state_answer)
 
-map = pandas.read_csv("50_states.csv")
-user_guess_state = ""
-for state in map:
-    if user_guess_state == state:
-        user_guess_state.title()
 
+map_data = pandas.read_csv("50_states.csv")
+all_states = map_data.state.to_list()
+user_guess_state = ""
+for state_answer in all_states:
+    if user_guess_state == state_answer:
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+
+
+turtle.onscreenclick(add_states_on_map)
 
 turtle.mainloop()
 
