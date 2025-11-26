dd42446636da2401e1e1c3c7be1f48dea5381c55
Taeyeon Kim <taekim1291@gmail.com>
2025-11-26 09:15:04 -0700

    main

---
+++ 100 Days of Code - The Complete Python Pro Bootcamp/day-25-start/day-25-us-states-game-start/main.py
@@ -0,0 +1,13 @@
+import turtle
+
+screen = turtle.Screen()
+screen.title("U.S States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+# with open("50_states.csv") as m:
+#
+
+
+screen.exitonclick()
