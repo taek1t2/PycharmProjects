8dd15b43a427292eb7e78ecb1f1b7f5592376abb
Taeyeon Kim <taekim1291@gmail.com>
2025-11-26 14:41:50 -0500

    main

--- 100 Days of Code - The Complete Python Pro Bootcamp/day-25-start/day-25-us-states-game-start/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/day-25-start/day-25-us-states-game-start/main.py
@@ -8,25 +8,19 @@ screen.addshape(image)
 turtle.shape(image)
 
 
-
-
-
 state_answer = screen.textinput(title= "Guess the State",
                                 prompt="What's another state's name?".title())
-print(state_answer)
-
 
 map_data = pandas.read_csv("50_states.csv")
 all_states = map_data.state.to_list()
-user_guess_state = ""
-for state_answer in all_states:
-    if user_guess_state == state_answer:
-        t = turtle.Turtle()
-        t.hideturtle()
-        t.penup()
 
+t = turtle.Turtle()
+t.hideturtle()
+t.penup()
+data = map_data[map_data.state == state_answer]
+t.goto(data.x.item(), data.y.item())
+t.write(state_answer)
 
-turtle.onscreenclick(add_states_on_map)
 
 turtle.mainloop()
 
