f32032f1e18abc5ae6aaed24f586fadf9497b5a6
Taeyeon Kim <taekim1291@gmail.com>
2025-11-28 13:43:16 -0500

    main

---
+++ 100 Days of Code - The Complete Python Pro Bootcamp/day-26-start/main.py
@@ -0,0 +1,21 @@
+
+#List Comprehension [new_item for item in list]
+#python sequence - range, tuple, list, strings
+
+numbers = [1, 2, 3]
+new_list = [n * 2 for n in numbers]
+print(new_list)
+
+
+my_name = "Taeyeon Kim"
+each_alpha = [letter for letter in my_name]
+print(each_alpha)
+
+list_in_range = range(1, 5)
+new_range_list = [num * 2 for num in list_in_range]
+print(new_range_list)
+
+#or
+
+new_list_num = [new_num * 2 for new_num in range(1, 8)]
+print(new_list_num)
