b9cb59d0979d2b695e84e86b6ad62fa5f30a73e7
Taeyeon Kim <taekim1291@gmail.com>
2025-11-30 16:23:14 -0700

    main

--- 100 Days of Code - The Complete Python Pro Bootcamp/day-25-start/day-25-us-states-game-start/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/day-25-start/day-25-us-states-game-start/main.py
@@ -17,13 +17,11 @@ while len(guessed_states) < 50:
     all_states = map_data.state.to_list()
 
     if state_answer == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
-            new_data = pandas.DataFrame(missing_states)
-            new_data.to_csv("states_to_learn")
+        missing_states = [state for state in all_states if state not in guessed_states]
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn")
         break
+
     if state_answer in all_states:
         guessed_states.append(state_answer)
         t = turtle.Turtle()
