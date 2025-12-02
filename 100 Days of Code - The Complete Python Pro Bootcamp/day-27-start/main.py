ed9b5bc2bdd0514c404bc0bfe83c5d0395b95416
Taeyeon Kim <taekim1291@gmail.com>
2025-12-02 14:29:29 -0700

    main, playground

--- 100 Days of Code - The Complete Python Pro Bootcamp/day-27-start/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/day-27-start/main.py
@@ -1,17 +1,30 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 
 window.title("My First GUI Program")
 window.minsize(width=400, height=300)
 
-#Label
-my_label = tkinter.Label(text= "Label Me", font=("Arial", 24, "bold"))
-my_label.pack(side = "left")
+#label
+my_label = Label(text="Some Text", font=("Arial", 24, "bold"))
+my_label.pack()
+
+my_label["text"] = "new text"
+my_label.config(text="new text")
+
+#button
+def button_clicked():
+    new_text = input.get()
+    my_label.config(text=new_text)
 
 
+button = Button(text="Click Me", command=button_clicked)
+button.pack()
 
-
+#Entry
+input = Entry(width=20)
+input.pack()
+input.get()
 
 
 
--- 100 Days of Code - The Complete Python Pro Bootcamp/day-27-start/playground.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/day-27-start/playground.py
@@ -2,6 +2,31 @@ def add(*args):
     sum = 0
     for any_num in args:
         sum += any_num
+    return sum
+
+print(add(100, 200, 300))
+
+def calculate(n, **kwargs):
+    # for key, value in kwargs.items():
+    #     print(key)
+    #     print(value)
+
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+
+calculate(5, add=3, multiply=5)
+
+
+def test(*args):
     print(args)
 
-add(100, 200, 300)
+
+test(1, 2, 3, 5) #output becomes a tuple
+
+
+def all_aboard(a, *args, **kw):
+    print(a, args, kw)
+
+
+all_aboard(4, 7, 3, 0, x=10, y=64) #args becomes tuple and kwargs become dictionary
+
