353217c3ffe9e541c794d256f474f323bf6ca2b6
Taeyeon Kim <taekim1291@gmail.com>
2025-12-02 19:28:27 -0700

    main, converter_main

---
+++ 100 Days of Code - The Complete Python Pro Bootcamp/day-27-start/converter_main.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+window = Tk()
+window.title("Mile to Km Converter")
+window.minsize(width=350, height=200)
+
+
+
+
+window.mainloop()
--- 100 Days of Code - The Complete Python Pro Bootcamp/day-27-start/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/day-27-start/main.py
@@ -7,10 +7,10 @@ window.minsize(width=400, height=300)
 
 #label
 my_label = Label(text="Some Text", font=("Arial", 24, "bold"))
-my_label.pack()
-
-my_label["text"] = "new text"
 my_label.config(text="new text")
+# my_label.pack(side="left")
+# my_label.place(x=0, y=0)
+my_label.grid(column=0, row=0)
 
 #button
 def button_clicked():
@@ -19,11 +19,15 @@ def button_clicked():
 
 
 button = Button(text="Click Me", command=button_clicked)
-button.pack()
+# button.pack(side="left")
+button.grid(column=1, row=1)
+new_button = Button(text="Click the 2nd Me")
+new_button.grid(column=2, row=0)
 
 #Entry
 input = Entry(width=20)
-input.pack()
+# input.pack(side="left")
+input.grid(column=3, row=2)
 input.get()
 
 #Text
