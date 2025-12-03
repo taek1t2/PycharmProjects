ca2e9d01ebf57f5b3dfe8066422817af5c82b12d
Taeyeon Kim <taekim1291@gmail.com>
2025-12-03 13:39:54 -0700

    converter_main

--- 100 Days of Code - The Complete Python Pro Bootcamp/day-27-start/converter_main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/day-27-start/converter_main.py
@@ -4,14 +4,14 @@ FONT = ("Arial", 13)
 
 window = Tk()
 window.title("Mile to Km Converter")
-window.minsize(width=350, height=200)
+window.minsize(width=350, height=100)
 window.config(padx=20, pady=20)
 window.columnconfigure(1, weight=1)
 # window.rowconfigure(0, weight=1)
 
 
 equal_to_label = Label(text="Is Equal To", font=FONT)
-equal_to_label.grid(column=0, row=3)
+equal_to_label.grid(column=0, row=1)
 
 miles = Label(text="Miles", font=FONT)
 miles.grid(column=2, row=0)
@@ -21,6 +21,12 @@ input.grid(column=1, row=0)
 input.get()
 
 calculate = Button(text="Calculate")
+calculate.grid(column=1, row=2)
 
+kilom_result_label = Label(text="0")
+kilom_result_label.grid(column=1, row=1)
+
+kilom_label = Label(text="Km", font=FONT)
+kilom_label.grid(column=2, row=1)
 
 window.mainloop()
