f3c7a20c3004b9d6462e03859162bd4065fae504
Taeyeon Kim <taekim1291@gmail.com>
2025-12-07 17:50:01 -0700

    main

--- 100 Days of Code - The Complete Python Pro Bootcamp/day-29-start/password-manager-start/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/day-29-start/password-manager-start/main.py
@@ -1,12 +1,30 @@
 from tkinter import *
+import random
+from char import letters, numbers, symbols
 FONT = ("Arial", 12)
 
+PASSWORD_LENGTH = 12
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    char_options = []
+    all_chars = letters, numbers, symbols
+    char_list = char_options.append(random.choice(all_chars))
+    for each in range(all_chars):
 
 
+    random.shuffle(char_options)
+    make_password = "".join(char_options)
+    return make_password
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save_password():
+    website = website_input.get()
+    password = password_input.get()
+    user = user_input.get()
 
+    with open("data.txt", "a") as df:
+        df.write(f"{website} | {user} | {password}")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -43,11 +61,11 @@ password_input = Entry(width=21)
 password_input.grid(column=1, row=4, sticky="w")
 
 #button generate password
-new_pass_button = Button(text="Generate Password")
+new_pass_button = Button(text="Generate Password", command=generate_password)
 new_pass_button.grid(column=2, row=4)
 
 #add button
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=36, command=save_password)
 add_button.grid(column=1, row=5, sticky="w",columnspan=2)
 
 
