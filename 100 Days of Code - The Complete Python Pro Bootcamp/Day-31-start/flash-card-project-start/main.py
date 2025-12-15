5546c817ffc912a9495151965d02e760056f36ae
Taeyeon Kim <taekim1291@gmail.com>
2025-12-15 15:23:35 -0700

    main

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day-31-start/flash-card-project-start/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day-31-start/flash-card-project-start/main.py
@@ -9,10 +9,15 @@ box.config(padx=50, pady=50, background=BACKGROUND_COLOR)
 
 box_canvas = Canvas(height=600, width=900)
 ft_card = PhotoImage(file="./images/card_front.png")
-box_canvas.create_image(263, 400, image=ft_card)
+box_canvas.create_image(450, 300, image=ft_card)
 box_canvas.grid(row=0, column=0)
 
 right = PhotoImage(file="./images/right.png")
-right_button = Button(image=right)
+right_button = Button(image=right, background=BACKGROUND_COLOR, highlightthickness=0)
+right_button.grid(row=1, column=1, columnspan=1)
+
+wrong = PhotoImage(file="./images/wrong.png")
+wrong_button = Button(image=wrong, highlightthickness=0, background=BACKGROUND_COLOR)
+wrong_button.grid(row=1, column=0, columnspan=1)
 
 box.mainloop()
