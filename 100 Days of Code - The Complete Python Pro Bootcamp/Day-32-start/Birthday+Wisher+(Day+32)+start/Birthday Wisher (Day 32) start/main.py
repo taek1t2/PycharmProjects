edf8b2feb3caecbac8d7e768f7bc165f0c1ea1a1
Taeyeon Kim <taekim1291@gmail.com>
2025-12-23 15:45:06 -0700

    main

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day-32-start/Birthday+Wisher+(Day+32)+start/Birthday Wisher (Day 32) start/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day-32-start/Birthday+Wisher+(Day+32)+start/Birthday Wisher (Day 32) start/main.py
@@ -1,4 +1,6 @@
 import smtplib
+import datetime as dt
+import random
 
 my_email = "taekim1291@gmail.com"
 password = "ogarinofnanoawwb"
@@ -12,13 +14,25 @@ with smtplib.SMTP('smtp.gmail.com', port=587) as connection:
     )
 
 
-import datetime as dt
-
 now = dt.datetime.now()
-year = now.year
-month = now.month
+# year = now.year
+# month = now.month
 day_of_week = now.weekday()
-print(day_of_week)
+# print(day_of_week)
+#
+# date_of_birth = dt.datetime(year=1990 , month=1 , day=2)
+# print(date_of_birth)
 
-date_of_birth = dt.datetime(year=1990 , month=1 , day=2)
-print(date_of_birth)
+with open("quotes.txt") as quotes:
+    all_quotes = quotes.readlines()
+    quote = random.choice(all_quotes)
+
+    print(quote)
+
+with smtplib.SMTP('smtp.gmail.com', port=587) as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(from_addr=my_email,
+                        to_addrs="taekim1291@gmail.com",
+                        msg=f"Subject:Tuesday Motivation \n\n {quote}"
+                        )
