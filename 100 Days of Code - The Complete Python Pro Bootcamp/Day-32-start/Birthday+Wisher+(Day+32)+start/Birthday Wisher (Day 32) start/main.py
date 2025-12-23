a7f881e4737be53a2299c77fade57a0796824017
Taeyeon Kim <taekim1291@gmail.com>
2025-12-23 15:11:51 -0700

    main

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day-32-start/Birthday+Wisher+(Day+32)+start/Birthday Wisher (Day 32) start/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day-32-start/Birthday+Wisher+(Day+32)+start/Birthday Wisher (Day 32) start/main.py
@@ -1,10 +1,9 @@
 import smtplib
 
 my_email = "taekim1291@gmail.com"
-password = "abcd1234()"
+password = "ogarinofnanoawwb"
 
-connection = smtplib.SMTP('smtp.gmail.com')
-connection.starttls()
-connection.login(user=my_email, password=password)
-connection.sendmail(from_addr=my_email, to_addrs="taekim1291@gmail.com", msg="Hello 2nd brain")
-connection.close()
+with smtplib.SMTP('smtp.gmail.com', port=587) as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(from_addr=my_email, to_addrs="taekim1291@gmail.com", msg="Hello 2nd brain")
