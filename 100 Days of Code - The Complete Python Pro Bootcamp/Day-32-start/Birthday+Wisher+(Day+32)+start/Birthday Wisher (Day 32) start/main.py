6f8cef63efc63d73fc2e8ea9c57f1ca93a7eb220
Taeyeon Kim <taekim1291@gmail.com>
2025-12-23 15:26:20 -0700

    main

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day-32-start/Birthday+Wisher+(Day+32)+start/Birthday Wisher (Day 32) start/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day-32-start/Birthday+Wisher+(Day+32)+start/Birthday Wisher (Day 32) start/main.py
@@ -6,4 +6,19 @@ password = "ogarinofnanoawwb"
 with smtplib.SMTP('smtp.gmail.com', port=587) as connection:
     connection.starttls()
     connection.login(user=my_email, password=password)
-    connection.sendmail(from_addr=my_email, to_addrs="taekim1291@gmail.com", msg="Hello 2nd brain")
+    connection.sendmail(from_addr=my_email,
+                        to_addrs="taekim1291@gmail.com",
+                        msg="Hello\n\n It's me, your 2nd brain"
+    )
+
+
+import datetime as dt
+
+now = dt.datetime.now()
+year = now.year
+month = now.month
+day_of_week = now.weekday()
+print(day_of_week)
+
+date_of_birth = dt.datetime(year=1990 , month=1 , day=2)
+print(date_of_birth)
