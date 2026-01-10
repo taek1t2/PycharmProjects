21e11fba13dad5b3985e79a028fd9b9b98474f9a
Taeyeon Kim <taekim1291@gmail.com>
2026-01-10 15:16:32 -0700

    main

--- 100 Days of Code - The Complete Python Pro Bootcamp/day-33-start/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/day-33-start/main.py
@@ -33,8 +33,10 @@ response.raise_for_status()
 data = response.json()
 
 sunrise = data['results']['sunrise']
-sunset = data['results']['sunset']
+sunset = data['results']['sunset'].split("T")[1].split(":")[0]
 print(sunrise)
+print(sunrise.split("T")[1].split(":"))
+print(sunset)
 
 time_now = datetime.now()
-print(time_now)
+# print(time_now)
